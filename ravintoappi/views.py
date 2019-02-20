361b6c3b5359196b7f1c9180218a96863076be5d
Wallinmaa <katriina.wallinmaa@cgi.com>
2019-02-20 23:59:04 +0200

    Tehty erillinen HTML-etusivunäkymä

--- ravintoappi/views.py
+++ ravintoappi/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Tervetuloa Ravintoappiin")
+    #return HttpResponse("Tervetuloa Ravintoappiin")
+    return render(request, 'etusivu.html', {})
+
+    #response = requests.get('https://fineli.fi/fineli/api/v1/foods/4019') #fineli tietokannasta kuoritun banaanin (elintarvike id=4019) ravintosisältö
+    #geodata = response.json()
+    #return render(request, 'core/home.html', {
+    #    'ip': geodata['ip'],
+    #    'country': geodata['country_name']
+    #})
