c4053e7f929372ca1b6bd66113198f6c7fb7555d
Wallinmaa <katriina.wallinmaa@cgi.com>
2019-03-07 22:34:02 +0200

    saatu APIsta tulostettua ensimmäinen viihdetapahtuma viikolla 11

--- menoappi/views.py
+++ menoappi/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-import requests
+import requests, json
 
 def index(request):
-    return render(request, 'etusivu.html', {})
+    response = requests.get('https://visittampere.fi:443/api/v1/event?tag=viihde&start_datetime=1552255200000&end_datetime=1552773600000')
+    tapahtumat = response.json()
+    return render(request, 'etusivu.html', {
+    'id' : tapahtumat[0]['title']
+
     #response = requests.get('https://fineli.fi/fineli/api/v1/foods/11049')
     #ravintosisalto = response.json()
     #return render(request, 'etusivu.html', {
