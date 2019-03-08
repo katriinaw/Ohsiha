b2024bb6e604126bc38104210ebcd018d1903ed4
Wallinmaa <katriina.wallinmaa@cgi.com>
2019-03-08 19:39:20 +0200

    Tapahtumalistaus toimii API:sta

--- menoappi/views.py
+++ menoappi/views.py
@@ -5,7 +5,8 @@ def index(request):
     response = requests.get('https://visittampere.fi:443/api/v1/event?tag=viihde&start_datetime=1552255200000&end_datetime=1552773600000')
     tapahtumat = response.json()
     return render(request, 'etusivu.html', {
-    'id' : tapahtumat[0]['title']
+    'tapahtumalista' : tapahtumat
+    })
 
     #response = requests.get('https://fineli.fi/fineli/api/v1/foods/11049')
     #ravintosisalto = response.json()
