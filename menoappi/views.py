585c0f6e320c49ea7539ffd3033631e55d6c8bbd
Wallinmaa <katriina.wallinmaa@cgi.com>
2019-04-02 00:02:59 +0300

    Etusivunäkymä toimii, haun rekisteröinti ei

--- menoappi/views.py
+++ menoappi/views.py
@@ -2,8 +2,14 @@ from django.shortcuts import render
 import requests, json
 
 def index(request):
-    response = requests.get('https://visittampere.fi:443/api/v1/event?tag=viihde&start_datetime=1552255200000&end_datetime=1552773600000')
-    tapahtumat = response.json()
-    return render(request, 'etusivu.html', {
-    'tapahtumalista' : tapahtumat
-    })
+    if 'hakusana' in request.POST:
+        hakusana = request.POST['hakusana']
+        url = "https://visittampere.fi:443/api/v1/event?tag="+hakusana+"&start_datetime=1552255200000&end_datetime=1552773600000"
+        response = requests.get(url)
+        tapahtumat = response.json()
+        return render(request, 'etusivu.html', {
+        'tapahtumalista' : tapahtumat
+        })
+    else:
+        return render(request, 'etusivu.html', {
+        })
